282951606ab863d5a9ac372e246988a53bab2d34
Oleksii Mykhailov <alem.coding@gmail.com>
2020-04-27 18:24:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3eae33d7ac1f65680fe9ea5b458330e.js"
+  "./precache-manifest.c54ae7b2b11e444c2ea7abfb520cec93.js"
 );
 
 self.addEventListener('message', (event) => {
